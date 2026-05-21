a474c99545a104a283ca496d775206de2457ebaa
IkwueInalegwu <ikwueinalegwu@gmail.com>
2026-05-21 11:29:32 +0100

    working on showing partners at on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@
 import { useScrollBlur } from "@/lib/hooks";
 import { HamburgerMenu } from "@solar-icons/react/ssr";
 import Link from "next/link";
-import { DropdownMenu } from "radix-ui";
+import { DropdownMenu, HoverCard } from "radix-ui";
 import ContactForm from "./contact-form";
 
 export default function Header() {
@@ -11,15 +11,28 @@ export default function Header() {
 
   return (
     <div
-      className={`fixed z-20 w-full flex items-center bg-transparent justify-between py-5 px-10 transition ${isScrolled ? "backdrop-blur-md" : "backdrop-blur-none"}`}
+      className={`fixed z-30 w-full flex items-center bg-transparent justify-between py-5 px-10 transition ${isScrolled ? "backdrop-blur-md" : "backdrop-blur-none"}`}
     >
       <Link href="/" className="font-title text-3xl">
         Axis
       </Link>
-      <nav className="hidden md:lg:xl:flex items-center justify-center gap-3">
+      <nav className="hidden md:lg:xl:flex items-center justify-center gap-5">
         <Link href="/studies" className="navlink">
           Case Studies
         </Link>
+        <HoverCard.Root>
+          <HoverCard.Trigger asChild>
+            <span className="navlink cursor-pointer">Partners</span>
+          </HoverCard.Trigger>
+          <HoverCard.Portal>
+            <HoverCard.Content
+              sideOffset={5}
+              className={`w-120 transition-all z-40 rounded-sm ${isScrolled ? "bg-background-950 border border-solid border-background-900/50 text-foreground-50" : "bg-background-100 border border-solid border-background-300/50 text-foreground-950"} p-5 shadow-[hsl(206_22%_7%/35%)_0px_10px_38px_-10px,hsl(206_22%_7%/20%)_0px_10px_20px_-15px] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade data-[state=open]:transition-all`}
+            >
+              content
+            </HoverCard.Content>
+          </HoverCard.Portal>
+        </HoverCard.Root>
       </nav>
       <nav className="flex md:lg:xl:hidden items-center justify-end gap-3">
         <DropdownMenu.Root>
